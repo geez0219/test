3a2d0c2b4add3d709c12ff42ab25257d08cdc5c8
Yun-Chan Tsai <geez0219@gmail.com>
2020-02-06 12:23:24 -0800

    Update install_dependency.sh

--- install_dependency.sh
+++ install_dependency.sh
@@ -2,7 +2,7 @@ ANACONDA_DIR=/home/ubuntu/anaconda3
 JENKINS_ENV_DIR=/var/lib/jenkins/.conda/envs
 ENV_NAME=pr_env
 
-export PATH=$ANACONDA_DIR/bin:PATH
+export PATH=$ANACONDA_DIR/bin:$PATH
 if [ ! -d "$JENKINS_ENV_DIR/$ENV_NAME" ]; then
     conda create --name $ENV_NAME python=3.6 -y
 fi
